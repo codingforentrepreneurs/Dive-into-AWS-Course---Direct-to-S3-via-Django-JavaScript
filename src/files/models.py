0aefde77ee7722a26825fe3d2e3da616046d613f
Justin Mitchel <hello@teamcfe.com>
2018-12-16 12:23:25 -0800

    7 - Why not FileField or ImageField

--- src/files/models.py
+++ src/files/models.py
@@ -4,9 +4,15 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to bucket/user_<id>/<filename>
+    return 's3files/user_{0}/{1}'.format(instance.user.id, filename)
+
+
 class S3File(models.Model):
     user        = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     name        = models.CharField(max_length=220, blank=True, null=True)
+    #thumbnail   = models.FileField(upload_to=user_directory_path, blank=True, null=True) # ImageField
     key         = models.TextField() # key path in aws "path"
     filetype    = models.CharField(max_length=120, default='video/mp4')
     uploaded    = models.BooleanField(default=False) # true / false
